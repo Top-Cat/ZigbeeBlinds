d3dfaff32ab2a03ecb3a34a313f203dbcc72dbe2
Thomas Cheyney <top_cat@thomasc.co.uk>
2026-03-27 10:37:33 +0000

    Prevent flickering from identify led

--- main/sensor.cpp
+++ main/sensor.cpp
@@ -252,7 +252,9 @@ void ZigbeeSensor::onConnect() {
 void ZigbeeSensor::zbAttributeSet(const esp_zb_zcl_set_attr_value_message_t *message) {
     if (message->info.cluster == ESP_ZB_ZCL_CLUSTER_ID_IDENTIFY) {
         uint16_t state = *(uint16_t *)message->attribute.data.value;
+        gpio_hold_dis(LED_PIN);
         gpio_set_level(LED_PIN, state % 2);
+        gpio_hold_en(LED_PIN);
     } else if (message->info.cluster == ESP_ZB_ZCL_CLUSTER_ID_WINDOW_COVERING) {
         switch (message->attribute.id) {
             case ESP_ZB_ZCL_ATTR_WINDOW_COVERING_VELOCITY_ID:
